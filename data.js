52fe30cfbd0c29828cbe8aab35286811a188c928
Daniel <dm375211@gmail.com>
2023-01-12 18:38:45 -0500

    Excercises

--- data.js
+++ data.js
@@ -1,31 +1,31 @@
-// export class Animal {
-//     constructor(type, legs) {
-//         this.type = type;
-//         this.legs = legs;
-//     }
+export class Animal {
+    constructor(type, legs) {
+        this.type = type;
+        this.legs = legs;
+    }
 
-//     makeNoise(sound = "Loud Noise") {
-//         console.log(sound);
-//     }
+    makeNoise(sound = "Loud Noise") {
+        console.log(sound);
+    }
 
-//     get metaData() {
-//         return `Type: ${this.type}, Legs: ${this.legs}`
-//     }
+    get metaData() {
+        return `Type: ${this.type}, Legs: ${this.legs}`
+    }
 
-//     static return10() {
-//         return 10;
-//     }
-// }        
+    static return10() {
+        return 10;
+    }
+}        
 
-// export class Cat extends Animal {
-//     // constructor (type, legs, tail) {
-//     //     super(type, legs);
-//     //     this.tail = tail;
-//     // }
-//     makeNoise(sound = "Meow") {
-//         console.log(sound);
-//     }  
-// }
+export class Cat extends Animal {
+    // constructor (type, legs, tail) {
+    //     super(type, legs);
+    //     this.tail = tail;
+    // }
+    makeNoise(sound = "Meow") {
+        console.log(sound);
+    }  
+}
 
 // Create a class Player with the following:
 // - Add a Name and Country properties
@@ -38,24 +38,24 @@
 // Make sure the Name and Age are dynamic.
 
 
-// export class Player {
-//     constructor (name, country) {
-//         this.nombre = name;
-//         this.pais = country;
-//     }
+export class Player {
+    constructor (name, country) {
+        this.nombre = name;
+        this.pais = country;
+    }
 
-//     nacimiento () {
-//         console.log(`${this.nombre} was born in ${this.pais}`);
-//     }
-// }
+    nacimiento () {
+        console.log(`${this.nombre} was born in ${this.pais}`);
+    }
+}
 
-// export class TennisPlayer extends Player {
-//     constructor (name, country, age) {
-//         super(name, country)
-//         this.edad = age;
-//     }
+export class TennisPlayer extends Player {
+    constructor (name, country, age) {
+        super(name, country)
+        this.edad = age;
+    }
 
-//     info() {
-//         console.log(`${this.nombre} is ${this.edad} years old and knows how to play Tennis`);
-//     }
-// }
+    info() {
+        console.log(`${this.nombre} is ${this.edad} years old and knows how to play Tennis`);
+    }
+}
--- index.js
+++ index.js
@@ -1,76 +1,73 @@
-// function addressMaker(address) {
-//     const { city, state } = address;
-//     const newAddress = {
-//         city,
-//         state,
-//         country: "USA"
-//     };
+function addressMaker(address) {
+    const { city, state } = address;
+    const newAddress = {
+        city,
+        state,
+        country: "USA"
+    };
 
-//     console.log(newAddress);
-// }
+    console.log(newAddress);
+}
 
-// addressMaker({city: "Austin", state: "Texas"})
+addressMaker({city: "Austin", state: "Texas"})
 
 
-// let fullName = "Daniel Mera"
-// let total = 0;
+let fullName = "Daniel Mera"
+let total = 0;
 
-// for (const char of fullName) {
-//     console.log(char);
-// }
+for (const char of fullName) {
+    console.log(char);
+}
 
-// const students = [
-//     {name: "John", city: "New York"},
-//     {name: "Peter", city: "Paris"},
-//     {name: "Katy", city: "Sidney"}
-// ]
+const students = [
+    {name: "John", city: "New York"},
+    {name: "Peter", city: "Paris"},
+    {name: "Katy", city: "Sidney"}
+]
 
-// for (const student of students) {
-//     const { name, city } = student;
-//     console.log(`${name} lives in ${city}`);
-// }
+for (const student of students) {
+    const { name, city } = student;
+    console.log(`${name} lives in ${city}`);
+}
 
-// let shoppingList = ["eggs", "milk", "butter"];
+let shoppingList = ["eggs", "milk", "butter"];
 
-// let shoppingBasket = [...shoppingList, "fruits", "cereal"];
+let shoppingBasket = [...shoppingList, "fruits", "cereal"];
 
-// console.log(shoppingBasket);
+console.log(shoppingBasket);
 
-// const buying = (food = "something") => console.log(`I'm going to buy ${food} from the grocery shop`);
+const buying = (food = "something") => console.log(`I'm going to buy ${food} from the grocery shop`);
 
-// buying("cocacola");
+buying("cocacola");
 
-// const listIngredients = ["flour", "sugar", "eggs", "butter"];
+const listIngredients = ["flour", "sugar", "eggs", "butter"];
 
-// if (listIngredients.includes("chocolate")) {
-//     console.log("We are going to make a chocolate cake");
-// } else {
-//     console.log("We can't make a chocolate cake because we are missing the ingredient chocolate");
-// }
+if (listIngredients.includes("chocolate")) {
+    console.log("We are going to make a chocolate cake");
+} else {
+    console.log("We can't make a chocolate cake because we are missing the ingredient chocolate");
+}
 
-// import { add } from './data.js';
+import { add } from './data.js';
 
-// let result = add(3, 2);
+let result = add(3, 2);
 
-// console.log(result);
+console.log(result);
 
-// let example = "Daniel Mera"
+let example = "Daniel Mera"
 
-// console.log(example.padStart(5, M));
+console.log(example.padStart(5, M));
 
-// export const add = (a, b) => {
-//     return a + b;
-// };
-// import { Animal, Cat } from "./data.js";
+import { Animal, Cat } from "./data.js";
 
-// let cat = new Cat("Felino", 4);
+let cat = new Cat("Felino", 4);
 
-// cat.makeNoise();
-// cat.type = 3;
-// console.log(cat.legs);
+cat.makeNoise();
+cat.type = 3;
+console.log(cat.legs);
 
-// console.log(Animal.return10());
-// cat.makeNoise()
+console.log(Animal.return10());
+cat.makeNoise()
 
 // Create a class Player with the following:
 // - Add a Name and Country properties
@@ -82,92 +79,92 @@
 // is 34 years old and knows how to play Tennis") ;
 // Make sure the Name and Age are dynamic.
 
-// import { Player, TennisPlayer } from "./data.js";
+import { Player, TennisPlayer } from "./data.js";
 
-// let messi = new Player("Messi", "Argentina");
+let messi = new Player("Messi", "Argentina");
 
-// messi.nacimiento();
+messi.nacimiento();
 
-// let rafael = new TennisPlayer("RAfael Nadal","sweden", 34);
+let rafael = new TennisPlayer("RAfael Nadal","sweden", 34);
 
-// rafael.info();
-// rafael.nacimiento();
+rafael.info();
+rafael.nacimiento();
 
-// function palindrome(str) {
+function palindrome(str) {
     
-//     let splitIt = (str.toLowerCase()).split("");
-//     let first = splitIt.join()
-//     console.log(first);
-//     let splitReverse = splitIt.reverse()
-//     let second = splitReverse.join()
-//     console.log(second);
+    let splitIt = (str.toLowerCase()).split("");
+    let first = splitIt.join()
+    console.log(first);
+    let splitReverse = splitIt.reverse()
+    let second = splitReverse.join()
+    console.log(second);
     
-//     if (first === second) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
+    if (first === second) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// palindrome("AaBaa")
+palindrome("AaBaa")
 
-// function encloseInBrackets(str) {
-//     const add = "(" + str + ")";
+function encloseInBrackets(str) {
+    const add = "(" + str + ")";
 
-//     console.log(add);
+    console.log(add);
 
-// }
+}
 
-// encloseInBrackets("alababa")
+encloseInBrackets("alababa")
 
-// function factorialNumber(numb) {
-//     let result = 1
-//     for (let i = 1; i <= numb; i++) {
-//        result *= i
-//        console.log(result);
-//     }
-// }
+function factorialNumber(numb) {
+    let result = 1
+    for (let i = 1; i <= numb; i++) {
+       result *= i
+       console.log(result);
+    }
+}
 
-// factorialNumber(5);
+factorialNumber(5);
 
-// function firstDigit(str) {
-//     let splited = str.split("")
-//     const numb = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+function firstDigit(str) {
+    let splited = str.split("")
+    const numb = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-//     for (let i = 0; i < splited.length; i++) {
-//        if (numb.includes(splited[i])) {
-//             console.log(splited[i]);
-//             break
-//        }
-//     }
+    for (let i = 0; i < splited.length; i++) {
+       if (numb.includes(splited[i])) {
+            console.log(splited[i]);
+            break
+       }
+    }
 
-// }
+}
 
-// firstDigit("estripat9m4ric4");
+firstDigit("estripat9m4ric4");
 
-// function largestNumber(num) {
-//     const nine = "9";
-//     let result = "0";
-//     for (let i = 0; i < num; i++) {
-//         result += nine
-//     }
-//     console.log(parseInt(result));
-// }
+function largestNumber(num) {
+    const nine = "9";
+    let result = "0";
+    for (let i = 0; i < num; i++) {
+        result += nine
+    }
+    console.log(parseInt(result));
+}
 
-// largestNumber(2);
+largestNumber(2);
 
-// function maxMultiple(divisor, bound) {
-//     let maximun = ""
-//     for (let i = 1; i <= bound; i++) {
-//         if (i % divisor === 0) {    
-//         maximun = i   
-//         }
-//     }
-//     console.log(maximun);
+function maxMultiple(divisor, bound) {
+    let maximun = ""
+    for (let i = 1; i <= bound; i++) {
+        if (i % divisor === 0) {    
+        maximun = i   
+        }
+    }
+    console.log(maximun);
 
-//     const remainder = bound % divisor;
+    const remainder = bound % divisor;
 
-//     console.log(bound - remainder);
-// }
+    console.log(bound - remainder);
+}
 
-// maxMultiple(5, 57);
+maxMultiple(5, 57);
